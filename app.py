0d4450084788d3d2f4de5a24c2167476899894f1
SIVASRI-2904 <81306533+SIVASRI-2904@users.noreply.github.com>
2022-09-27 18:59:19 +0530

    files upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['GET','POST'])
+def predict():
+    input = request.form.get('inp')
+    model = pickle.load(open('t5model.pkl','rb'))
+    res = model.predict(input)
+    res1=''
+    for i in res:
+        res1=res1+i
+    return render_template("res.html",res1=res1,input=input)#"GIVEN INPUT:\n"+input+"SUMMARY:\n"+res1#render_template("index.html")#,res1=res1,input=input)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
